1367f95f3afbc79af483d695bd49e104a4697df3
Anton Klimenko <antonyklimenko@gmail.com>
2022-06-23 21:10:02 +0300

    Testing code. Must be recreated later!

---
+++ Test/by/itstep/klimenkojavalessons/lesson20/model/logic/CalculateTest.java
@@ -0,0 +1,95 @@
+package by.itstep.klimenkojavalessons.lesson20.model.logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculateTest {
+    @Test
+
+    // Rule AAA!!!
+
+    public void testSum() {
+        // Arrange
+
+        int a = 10;
+        int b = 5;
+        int expected = 15;
+
+        // Act
+        int actual = Calculator.sum(a, b);
+
+        //Assert
+
+        if (expected != actual) {
+            Assert.fail();
+
+
+        }
+    }
+    @Test
+    public void testSun() {
+        // Arrange
+
+        int a = 10;
+        int b = 5;
+        int expected = 5;
+
+        // Act
+        int actual = Calculator.sun(a, b);
+
+        //Assert
+
+        if (expected != actual) {
+            Assert.fail();
+
+
+        }
+    }
+    @Test
+    public void testMul() {
+        // Arrange
+
+        int a = 10;
+        int b = 5;
+        int expected = 50;
+
+        // Act
+        int actual = Calculator.mul(a, b);
+
+        //Assert
+
+        if (expected != actual) {
+            Assert.fail();
+
+
+        }
+    }
+
+    @Test
+    public void testDiv() {
+        // Arrange
+
+        int a = 10;
+        int b = 5;
+        int expected = 2;
+
+        // Act
+        int actual = Calculator.div(a, b);
+
+        //Assert
+
+        if (expected != actual) {
+            Assert.fail();
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/by/itstep/klimenkojavalessons/lesson20/model/logic/Calculator.java
@@ -0,0 +1,28 @@
+package by.itstep.klimenkojavalessons.lesson20.model.logic;
+
+public class Calculator {
+    public static int sum(int a, int b) {
+
+
+        return a + b;
+
+    }
+
+    public static int sun(int a, int b) {
+        return a - b;
+
+    }
+
+
+    public static int mul(int a, int b) {
+        return a * b;
+
+    }
+
+
+    public static int div(int a, int b) {
+        return a / b;
+
+    }
+}
+
